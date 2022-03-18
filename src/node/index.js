128ec0d3e4c321bc9af19d40fb64fced7ceaa8f0
PresentKim <debe3721@gmail.com>
2022-03-18 12:00:58 +0900

    :ambulance: Add clientAppEnhanceFiles props for require client scripts

--- src/node/index.js
+++ src/node/index.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("@vuepress/utils");
 const giscusCommentPlugin = (options, app) => {
     return {
         name: 'vuepress-plugin-giscus-comment',
         define: {
             __GISCUS_COMMENT_OPTIONS__: options
-        }
+        },
+        clientAppEnhanceFiles: [
+            utils_1.path.resolve(__dirname, '../client')
+        ]
     };
 };
 exports.default = giscusCommentPlugin;
--- src/node/index.ts
+++ src/node/index.ts
@@ -1,4 +1,5 @@
 import type {Plugin} from '@vuepress/core';
+import {path} from '@vuepress/utils';
 import {GiscusCommentPluginOptions} from "../shared";
 
 const giscusCommentPlugin: Plugin<GiscusCommentPluginOptions> = (options, app) => {
@@ -6,7 +7,10 @@ const giscusCommentPlugin: Plugin<GiscusCommentPluginOptions> = (options, app) =
         name: 'vuepress-plugin-giscus-comment',
         define: {
             __GISCUS_COMMENT_OPTIONS__: options
-        }
+        },
+        clientAppEnhanceFiles: [
+            path.resolve(__dirname, '../client')
+        ]
     }
 };
 export default giscusCommentPlugin;
