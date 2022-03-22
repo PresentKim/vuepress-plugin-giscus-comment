985567a6ed512af0b723b0c79673554c27c18330
PresentKim <debe3721@gmail.com>
2022-03-22 13:17:55 +0900

    :truck: Mode builded files into lib/ directory

--- src/node/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Plugin } from '@vuepress/core';
-import { GiscusCommentPluginOptions } from "../shared";
-declare const giscusCommentPlugin: Plugin<GiscusCommentPluginOptions>;
-export default giscusCommentPlugin;
