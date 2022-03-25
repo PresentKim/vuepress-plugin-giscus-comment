3cff1429d0d4ebf781674495938e8d107cb8a256
PresentKim <debe3721@gmail.com>
2022-03-25 11:25:34 +0900

    :sparkles: Refactoring GiscusCommentPlugin into a class

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,16 +1,26 @@
-import type {Plugin} from '@vuepress/core';
+import type {Plugin, PluginObject} from '@vuepress/core';
 import {path} from '@vuepress/utils';
 import {GiscusCommentPluginOptions} from "../shared";
 
-const giscusCommentPlugin: Plugin<GiscusCommentPluginOptions> = (options, app) => {
-    return {
-        name: 'vuepress-plugin-giscus-comment',
-        define: {
-            __GISCUS_COMMENT_OPTIONS__: options
-        },
-        clientAppEnhanceFiles: [
-            path.resolve(__dirname, '../client')
-        ]
+let __GISCUS_COMMENT_OPTIONS__: Partial<GiscusCommentPluginOptions> = {};
+
+export class GiscusCommentPlugin implements PluginObject {
+    readonly name: string = 'vuepress-plugin-giscus-comment';
+
+    constructor(options: typeof __GISCUS_COMMENT_OPTIONS__) {
+        __GISCUS_COMMENT_OPTIONS__ = options;
     }
+
+    define() {
+        return {__GISCUS_COMMENT_OPTIONS__};
+    }
+
+    clientAppEnhanceFiles() {
+        return path.resolve(__dirname, '../client');
+    }
+}
+
+const plugin: Plugin<GiscusCommentPluginOptions> = (options, app) => {
+    return new GiscusCommentPlugin(options);
 };
-export default giscusCommentPlugin;
+export default plugin;
